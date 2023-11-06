e7806cbb87435f21ecd3284325dcd4a0fb1b02dc
frank-shema <shemafranko0987@gmail.com>
2023-11-06 16:38:39 +0200

    made changes

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import {
   RxDiscordLogo,
   RxGithubLogo,
-
+  RxInstagramLogo,
+  RxTwitterLogo,
+  RxLinkedinLogo,
 } from "react-icons/rx";
 
 import { FaYoutube } from "react-icons/fa";
--- components/sub/SkillText.tsx
+++ components/sub/SkillText.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import { motion } from 'framer-motion'
 import { slideInFromLeft, slideInFromRight, slideInFromTop } from '@/utils/motion'
